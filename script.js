8edaf36e2a817b753ab8b90d294f9bd6af38ff69
VitoMarino <marino.vito2001@gmail.com>
2024-05-22 16:03:54 +0200

    append

--- script.js
+++ script.js
@@ -4,31 +4,7 @@ const p = document.createElement('p');
 const article = document.createElement('article');
 const img = document.createElement('img');
 
-// Ciclo for per ripetizioni tag
-for (let i = 1; i <= 6; i++) {
-    const p = document.createElement('p');
-    const article = document.createElement('article');
-    const img = document.createElement('img');
-    
-    div.append(article);
-    article.append(p)
-    article.append(img);
-}
-
-// Aggiunta classi
-let articleUno = document.querySelector();
-let articleDue = document.querySelector();
-let articleTre = document.querySelector();
-let articleQuattro = document.querySelector();
-let articleCinque = document.querySelector();
-let articleSei = document.querySelector();
-
-articleUno.classList.add()
-articleDue.classList.add()
-articleTre.classList.add()
-articleQuattro.classList.add()
-articleCinque.classList.add()
-articleSei.classList.add()
+const paroleCasuali = ['ciao', 'mondo', 'gino', 'palla', 'gnocchi', 'albero'];
 
 // Array oggetti
 const team = [
@@ -73,4 +49,38 @@ const team = [
 for (let key in team) {
     console.log(team[key]);
 }
+
+// Ciclo for per ripetizioni tag
+for (let i = 0; i <= 5; i++) {
+    const p = document.createElement('p');
+    const article = document.createElement('article');
+    const img = document.createElement('img');
+    
+    div.append(article);
+    article.append(p)
+    article.append(img);
+
+    p.append(paroleCasuali);
+}
+
+
+
+// Aggiunta classi
+// let articleUno = document.querySelector(" div article:nth-child(1) ");
+// let articleDue = document.querySelector();
+// let articleTre = document.querySelector();
+// let articleQuattro = document.querySelector();
+// let articleCinque = document.querySelector();
+// let articleSei = document.querySelector();
+
+// articleUno.classList.add('ciao')
+// articleDue.classList.add()
+// articleTre.classList.add()
+// articleQuattro.classList.add()
+// articleCinque.classList.add()
+// articleSei.classList.add()
+
+
+
+
 
