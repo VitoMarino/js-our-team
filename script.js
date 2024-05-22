6ae2a82bb1bad3491cd2ed90d5edeeadda54f648
VitoMarino <marino.vito2001@gmail.com>
2024-05-22 17:26:40 +0200

    img

--- script.js
+++ script.js
@@ -2,50 +2,49 @@
 const div = document.querySelector('div');
 const p = document.createElement('p');
 const article = document.createElement('article');
-const img = document.createElement('img');
-
-const paroleCasuali = ['ciao', 'mondo', 'gino', 'palla', 'gnocchi', 'albero'];
+let img = document.createElement('img');
 
 // Array oggetti
 const team = [
     {
-        nome: 'Wayne Barnett',
-        ruolo: 'Founder & CEO',
-        foto: 'wayne-barnett-founder-ceo.jpg'
+        nome: 'Wayne Barnett ',
+        ruolo: 'Founder & CEO ',
+        foto: img.src = 'wayne-barnett-founder-ceo.jpg '
     },
     
     {
-        nome: 'Angela Caroll',
-        ruolo: 'Chief Editor',
-        foto: 'angela-caroll-chief-editor.jpg'
+        nome: 'Angela Caroll ',
+        ruolo: 'Chief Editor ',
+        foto: img.src = ' angela-caroll-chief-editor.jpg '
     },
 
     {
-        nome: 'Walter Gordon',
-        ruolo: 'Office Manager',
-        foto: '	walter-gordon-office-manager.jpg'
+        nome: 'Walter Gordon ',
+        ruolo: 'Office Manager ',
+        foto: img.src = 'walter-gordon-office-manager.jpg '
     },
 
     {
-        nome: 'Angela Lopez',
-        ruolo: 'Social Media Manager',
-        foto: 'angela-lopez-social-media-manager.jpg'
+        nome: 'Angela Lopez ',
+        ruolo: 'Social Media Manager ',
+        foto: img.src = 'angela-lopez-social-media-manager.jpg '
     },
 
     {
-        nome: 'Scott Estrada',
-        ruolo: 'Developer',
-        foto: 'scott-estrada-developer.jpg',
+        nome: 'Scott Estrada ',
+        ruolo: 'Developer ',
+        foto: img.src = 'scott-estrada-developer.jpg '
     },
 
     {
-        nome: 'Barbara Ramos',
-        ruolo: 'Graphic Designer',
-        foto: 'barbara-ramos-graphic-designer.jpg'
+        nome: 'Barbara Ramos ',
+        ruolo: 'Graphic Designer ',
+        foto: img.src = 'barbara-ramos-graphic-designer.jpg '
     }
 ];
 
 // Ciclo forin per array oggetti in console
+
 for (let key in team) {
     console.log(team[key]);
 }
@@ -60,27 +59,8 @@ for (let i = 0; i <= 5; i++) {
     article.append(p)
     article.append(img);
 
-    p.append(paroleCasuali);
+    p.append(team[i].nome, team[i].ruolo);
 }
 
-
-
-// Aggiunta classi
-// let articleUno = document.querySelector(" div article:nth-child(1) ");
-// let articleDue = document.querySelector();
-// let articleTre = document.querySelector();
-// let articleQuattro = document.querySelector();
-// let articleCinque = document.querySelector();
-// let articleSei = document.querySelector();
-
-// articleUno.classList.add('ciao')
-// articleDue.classList.add()
-// articleTre.classList.add()
-// articleQuattro.classList.add()
-// articleCinque.classList.add()
-// articleSei.classList.add()
-
-
-
-
+document.body.append(img);
 
